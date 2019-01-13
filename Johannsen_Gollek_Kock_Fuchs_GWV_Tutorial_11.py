710414d9e236ba13dc27289e8c5bea1e3f4086ec
babyheroe <45822479+babyheroe@users.noreply.github.com>
2019-01-13 15:50:11 +0100

    Add files via upload

---
+++ Johannsen_Gollek_Kock_Fuchs_GWV_Tutorial_11.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Dieses Dokument wurde von
+Malte Johannsen (6946244),
+Marc Fuchs (6054068),
+Sünje Gollek (6894597)
+& Marius Kock (6947933) 
+bearbeitet.
+
+GWV Tutorial 
+"""
+
+#import numpy as np
+
+#Aufgabe 1
+
+get_dict_value(Person1, Person2):
+    if Person1 in dictionary:
+        if Person2 in dictionary[Person1]:
+            return dictionary[Person1][Person2]
+    return 0
