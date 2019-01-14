481494dcb0723e82574bcc257876949a0c09afc1
babyheroe <45822479+babyheroe@users.noreply.github.com>
2019-01-14 07:10:11 +0100

    Grundfunktionen Datenauslesen
    Mit Hilfe dieser Funktion solltet ihr anfangen können zu programmieren. Wie ihr sie verwenden könnt, seht ihr im print.

--- Johannsen_Gollek_Kock_Fuchs_GWV_Tutorial_11.py
+++ Johannsen_Gollek_Kock_Fuchs_GWV_Tutorial_11.py
@@ -10,12 +10,43 @@ bearbeitet.
 GWV Tutorial 
 """
 
-#import numpy as np
-
 #Aufgabe 1
 
-get_dict_value(Person1, Person2):
+def get_dictionary():
+    data = open("Johannsen_Gollek_Kock_Fuchs_GWV_Tutorial_11_sitzordnung.txt", "r")
+
+    text =  data.readlines()
+
+    data.close()
+    
+    dictionary = {}
+    
+    for i in range(0, len(text)):
+        text[i] = str(text[i].strip('\n'))
+        if text[i] != '':
+            person1 = text[i].split(";")[0]
+            person2 = text[i].split(";")[1]
+            relation = text[i].split(";")[2]
+            
+            if person1 in dictionary and person2 not in dictionary[person1]:
+                dictionary[person1][person2] = relation
+            else: 
+                dictionary[person1] = {person2 : relation}
+            
+    return dictionary
+
+dictionary = get_dictionary()
+
+def get_dict_value(Person1, Person2):
     if Person1 in dictionary:
         if Person2 in dictionary[Person1]:
             return dictionary[Person1][Person2]
-    return 0
+        
+    elif Person2 in dictionary:
+        if Person1 in dictionary[Person2]:
+            return dictionary[Person2][Person1]
+    else:
+        return 0
+
+print dictionary
+print get_dict_value('Peter', 'Juergen')
